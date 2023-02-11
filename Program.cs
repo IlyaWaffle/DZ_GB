e17f5caf966b42149a83e2e4d4b62b59fc331e61
IlyaPC <krotov891@gmail.com>
2023-02-11 16:10:33 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int N;
+Console.Write("Введите число: ");
+bool flag = int.TryParse(Console.ReadLine()!, out N);
+
+if (flag)
+{
+    if (N < 0)
+        N *= -1;
+    for (int i = 0, j = N - 1; i < N; i++, j--)
+    {
+        Console.Write($"{i}{j} ");
+    }
+}
+else
+    Console.Write("Неверный ввод");
