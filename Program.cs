f696a4de6410fa27f1fe1c855fef61db5b0453d5
IlyaPC <krotov891@gmail.com>
2023-02-11 16:13:31 +0300

    task2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ if (flag)
 {
     if (N < 0)
         N *= -1;
-    for (int i = 0, j = N - 1; i < N; i++, j--)
+    for (int i = 0; i < N; i++)
     {
-        Console.Write($"{i}{j} ");
+        Console.Write($"{i * 2} ");
     }
 }
 else
